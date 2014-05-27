c432c87bff13c83507b8528777f4cbdde02fe33a
Douglas Busck <douglas.busck@gmail.com>
2014-05-27 18:03:50 +0200

    Featured image support

--- ifkvanersborg.se/themes/ifkvbg/front-page.php
+++ ifkvanersborg.se/themes/ifkvbg/front-page.php
@@ -1,15 +1,21 @@
 <?php get_header(); ?>
 
-		<section class="hero-image">
-	      <img src="<?php bloginfo( 'stylesheet_directory' ); ?>/images/startsida.jpg" alt="hero">
-	      <div class="positioner">
-	        <div class="table row">
-	          <div class="cell">
-	          <h1 class="hero-title panel"><a href="<?php the_permalink(); ?>"><h1 class="center"><?php the_title(); ?></h1></a></h1>
-	          </div>
-	        </div>
-	      </div>
-	    </section>
+		<?php if ( has_post_thumbnail() ): ?>
+			<section class="hero-image">
+				<?php the_post_thumbnail(); ?>
+				<div class="positioner">
+					<div class="table row">
+						<div class="cell">
+							<h1 class="hero-title panel"><a href="<?php the_permalink(); ?>"><h1 class="center"><?php the_title(); ?></h1></a></h1>
+						</div>
+					</div>
+				</div>
+			</section>
+		<?php else : ?>
+			<div class="row">
+				<h1 class="hero-title standalone-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+			</div>
+		<?php endif ?>
 	  	
 	  	<div class="panels-container">
 			<div class="row">
