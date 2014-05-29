3c389929790f482d576b3d0a3ddf98f23792bdfe
Julia Thielen <julia.thielen@hotmail.com>
2014-05-29 11:12:55 +0200

    Partners undersida

---
+++ ifkvanersborg.se/themes/ifkvbg/partners.php
@@ -0,0 +1,39 @@
+<?php
+/*
+Template Name: Partners
+*/
+
+<?php get_header(); ?>
+
+		<?php get_template_part( 'content-hero'); ?>
+	  	
+	  	<div class="content row">
+			<div class="panel medium-8 column">
+				<?php
+ 
+						// check if the repeater field has rows of data
+						if( have_rows('partners') ):
+						 
+						 	// loop through the rows of data
+						    while ( have_rows('partners') ) : the_row();
+						 
+						        // display a sub field value
+						        the_sub_field('partners_rubrik');
+						 
+						    endwhile;
+						 
+						else :
+						 
+						    // no rows found
+						 
+						endif;
+						 
+						?>
+
+
+				<?php get_template_part( 'content'); ?>
+			</div>
+			<?php get_sidebar(); ?>
+	  	</div>
+
+<?php get_footer(); ?>
