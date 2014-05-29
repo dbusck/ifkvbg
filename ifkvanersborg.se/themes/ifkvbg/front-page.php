dd2a5f052547f1a408a84e90bbafd8e8f8fab6ab
Douglas Busck <douglas.busck@gmail.com>
2014-05-29 15:12:38 +0200

    sponsorlogos dissapeared...

--- ifkvanersborg.se/themes/ifkvbg/front-page.php
+++ ifkvanersborg.se/themes/ifkvbg/front-page.php
@@ -106,8 +106,6 @@
 			   </div>
 			<?php endif; ?>
 
-			<?php get_template_part( 'instagram-feed'); ?>
-
 
 			<?php if( have_rows('front-page-blurbs') ):	?>
 				<div class="row">
@@ -128,5 +126,19 @@
    			</div>
    		<?php endif; ?>
       </div>
+
+	   <?php get_template_part( 'instagram-feed'); ?>
+
+	   <div class="front-page-sponsors sponsors text-center">
+	   	<?php if( have_rows('front-page-sponsors') ):	?>
+				<div class="row">
+	   			<?php	while ( have_rows('front-page-sponsors') ) : the_row(); ?>
+	   				<div class="sponsor-logo medium-6 large-3 column">
+							<img src="<?php the_sub_field('front-page-sponsor-logo'); ?>" alt="sponsor-logo">
+						</div>
+					<?php endwhile; ?>
+				</div>
+			<?php endif; ?>
+	   </div>
 
 <?php get_footer(); ?>
